9e2ae3d5838fe3f1e5cce107e3af66e379990b7c
Conningemu <krint@yahoo.com>
2020-06-19 18:49:58 -0400

    Metronome

---
+++ Assets/Scripts/Metronome.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Metronome : MonoBehaviour
+{
+    public double bpm = 140.0F;
+
+    public AudioClip tick;
+
+    double nextTick = 0.0F; // The next tick in dspTime
+    double sampleRate = 0.0F;
+    bool ticked = false;
+
+    void Start()
+    {
+        double startTick = AudioSettings.dspTime;
+        sampleRate = AudioSettings.outputSampleRate;
+
+        nextTick = startTick + (60.0 / bpm);
+    }
+
+    void LateUpdate()
+    {
+        if (!ticked && nextTick >= AudioSettings.dspTime)
+        {
+            ticked = true;
+            BroadcastMessage("OnTick");
+
+        }
+    }
+
+    // Just an example OnTick here
+    void OnTick()
+    {
+        Debug.Log("Tick");
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.clip = tick;
+        audio.Play();
+
+        
+
+
+        // GetComponent<AudioSource>().Play();
+    }
+
+    void FixedUpdate()
+    {
+        double timePerTick = 60.0f / bpm;
+        double dspTime = AudioSettings.dspTime;
+
+        while (dspTime >= nextTick)
+        {
+            ticked = false;
+            nextTick += timePerTick;
+        }
+
+    }
+}
