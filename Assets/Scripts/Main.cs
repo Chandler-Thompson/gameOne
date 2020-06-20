7d91ffc1efda214e7092834b287b94eb7754a482
Chandler <chandler.dfys@gmail.com>
2020-06-20 15:22:18 -0400

    Started Map Generation, Battle Management, and Player Objects
    +Initial (still buggy) Map Generator
    +Battle Manager that takes care of combat given two tiles
    +Player objects

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+
+    public MapGenerator mapGenerator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Starting generation...");
+        mapGenerator.beginGenerator();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
