3787f2b7745be23d1fb7dea7ef485bae6617634f
Chandler <chandler.dfys@gmail.com>
2020-06-20 21:34:06 -0400

    updated gitignore and readded HumanPlayer

---
+++ Assets/Scripts/HumanPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanPlayer : Competitor
+{
+
+    private Hex selectedTile = null;
+
+    public void setSelected(Hex hex){
+        this.selectedTile = hex;
+    }
+
+    public Hex getSelected(){
+        return this.selectedTile;
+    }
+
+}
